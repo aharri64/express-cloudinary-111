ca886b491439e6251b166071771c8dc2409b8329
Rome Bell <romebell5@gmail.com>
2021-02-05 17:49:35 -0800

    index: update

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const multer = require('multer');
 const cloudinary = require('cloudinary');
+// uploader for images, make a uploads folder, pass through the route as middleware
+const uploads = multer({ dest: './uploads'});
+
 // 3 - App Set up
 const app = express();
 app.set('view engine', 'ejs');
@@ -15,6 +18,28 @@ app.use(express.urlencoded({ extended: false })); // able to access req.body
 app.get('/', function(req, res) {
     res.send('Welcome to my Express Cloudinary App');
 });
+
+// Need this in various cases...
+// app.get('/images', (req, res) => {
+//     res.render('index');
+// })
+
+app.get('/images/new', (req, res) => {
+    res.render('new');
+});
+
+app.post('/images', uploads.single('inputFile'), (req, res) => {
+    // grab the uploaded file
+    const image = req.file.path;
+    console.log(image);
+    // upload image to cloudinary
+    cloudinary.uploader.upload(image, (result) => {
+        // the result that comes back from cloudinary
+        console.log(result);
+        res.render('index', { image: result.url })
+    })
+
+})
 
 // Listen on PORT 
 const PORT = process.env.PORT || 8000;
